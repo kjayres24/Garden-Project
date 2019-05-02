6ebefbb968c3ba59e81ca0a3f2e2d7c5b20a08e0
Katie Ayres <katieayres24@gmail.com>
2019-05-02 11:12:36 -0400

    added nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { AdminComponent } from './admin/admin.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AdminComponent } from './admin/admin.component';
     HomeComponent,
     AboutComponent,
     ProductsComponent,
-    AdminComponent
+    AdminComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
